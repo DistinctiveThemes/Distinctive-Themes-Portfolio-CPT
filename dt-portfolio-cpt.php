945c12da27f1c9635ca93bf9623e788a96293b5f
Danny Jones <danny@distinctivethemes.com>
2014-03-11 16:43:58 +0000

    Initial Upload

---
+++ dt-portfolio-cpt.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: Distinctive Themes Portfolio CPT
+Plugin URI: http://www.distinctivethemes.com
+Description: Portable Post Type from Distinctive Themes
+Author: Danny Jones (Distinctive Themes)
+Version: 1.0.0
+Author URI: http://www.distinctivethemes.com
+*/
+
+?>
