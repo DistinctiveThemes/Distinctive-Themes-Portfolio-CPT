6b18ae52869cd573769b361b2e72f44ccb07098f
Danny Jones <contact@distinctivethemes.com>
2014-03-11 21:18:35 +0000

    Added TAX

--- dt-portfolio-cpt.php
+++ dt-portfolio-cpt.php
@@ -52,4 +52,42 @@ function register_cpt_dt_portfolio_cpt() {
     register_post_type( 'dt_portfolio_cpt', $args );
 }
 
+add_action( 'init', 'register_taxonomy_dt_portfolio_cpt_categories' );
+
+function register_taxonomy_dt_portfolio_cpt_categories() {
+
+    $labels = array( 
+        'name' => _x( 'Project Categories', 'distinctpress' ),
+        'singular_name' => _x( 'Project Category', 'distinctpress' ),
+        'search_items' => _x( 'Search Project Categories', 'distinctpress' ),
+        'popular_items' => _x( 'Popular Project Categories', 'distinctpress' ),
+        'all_items' => _x( 'All Project Categories', 'distinctpress' ),
+        'parent_item' => _x( 'Parent Project Category', 'distinctpress' ),
+        'parent_item_colon' => _x( 'Parent Project Category:', 'distinctpress' ),
+        'edit_item' => _x( 'Edit Project Category', 'distinctpress' ),
+        'update_item' => _x( 'Update Project Category', 'distinctpress' ),
+        'add_new_item' => _x( 'Add New Project Category', 'distinctpress' ),
+        'new_item_name' => _x( 'New Project Category', 'distinctpress' ),
+        'separate_items_with_commas' => _x( 'Separate project categories with commas', 'distinctpress' ),
+        'add_or_remove_items' => _x( 'Add or remove project categories', 'distinctpress' ),
+        'choose_from_most_used' => _x( 'Choose from the most used project categories', 'distinctpress' ),
+        'menu_name' => _x( 'Project Categories', 'distinctpress' ),
+    );
+
+    $args = array( 
+        'labels' => $labels,
+        'public' => true,
+        'show_in_nav_menus' => true,
+        'show_ui' => true,
+        'show_tagcloud' => false,
+        'show_admin_column' => true,
+        'hierarchical' => true,
+
+        'rewrite' => true,
+        'query_var' => true
+    );
+
+    register_taxonomy( 'dt_portfolio_cpt_categories', array('dt_portfolio_cpt'), $args );
+}
+
 ?>
